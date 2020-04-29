9cd086bb2b6ba09e6defc3803f579cb026feb533
CNFINITE <1034989912@qq.com>
2020-04-29 17:15:21 +0800

    update files

--- src/crawler.py
+++ src/crawler.py
@@ -7,9 +7,6 @@ import ccxt
 pd.set_option('expand_frame_repr', False)  #
 
 TIMEOUT = 6  # 6 second
-BITFINEX_LIMIT = 5000
-BITMEX_LIMIT = 500
-BINANCE_LIMIT = 500
 
 
 def crawl_exchanges_datas(exchange_name, symbol, start_time, end_time):
@@ -25,7 +22,6 @@ def crawl_exchanges_datas(exchange_name, symbol, start_time, end_time):
     exchange_class = getattr(ccxt, exchange_name)  # 获取交易所的名称 ccxt.binance
     exchange = exchange_class()  # 交易所的类. 类似 ccxt.bitfinex()
     print(exchange)
-    # exit()
 
     current_path = os.getcwd()
     file_dir = os.path.join(current_path, exchange_name, symbol.replace('/', ''))
@@ -33,7 +29,6 @@ def crawl_exchanges_datas(exchange_name, symbol, start_time, end_time):
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)
 
-    # exit()
 
     start_time = datetime.datetime.strptime(start_time, '%Y-%m-%d')
     end_time = datetime.datetime.strptime(end_time, '%Y-%m-%d')
@@ -41,24 +36,21 @@ def crawl_exchanges_datas(exchange_name, symbol, start_time, end_time):
     start_time_stamp = int(time.mktime(start_time.timetuple())) * 1000
     end_time_stamp = int(time.mktime(end_time.timetuple())) * 1000
 
-    print(start_time_stamp)  # 1529233920000
-    print(end_time_stamp)
+    print('开始时间:', start_time_stamp)  # 1529233920000
+    print('结束时间:', end_time_stamp)
 
-    # exit()
-    limit_count = 500
-    if exchange_name == 'bitfinex':
-        limit_count = BITFINEX_LIMIT
-    elif exchange_name == 'bitmex':
-        limit_count = BITMEX_LIMIT
-    elif exchange_name == 'binance':
-        limit_count = BINANCE_LIMIT
+    limit_count = 100
+    if exchange_name == 'bitfinex' or 'bitfinex':
+        limit_count = 5000
+    elif exchange_name == 'huobipro':
+        limit_count = 2000
 
     while True:
         try:
             print(start_time_stamp)
             data = exchange.fetch_ohlcv(symbol, timeframe='1m', since=start_time_stamp, limit=limit_count)
             df = pd.DataFrame(data)
-            if exchange_name == 'huobi' or exchange_name == 'binance' or exchange_name == 'bittrex' or exchange_name == 'poloniex':
+            if exchange_name == 'huobipro' or exchange_name == 'binance' or exchange_name == 'bittrex' or exchange_name == 'poloniex':
                 df.rename(columns={0: 'open_time', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'basevolume',
                                    6: 'quotevolume'},
                           inplace=True)
@@ -72,7 +64,6 @@ def crawl_exchanges_datas(exchange_name, symbol, start_time, end_time):
             save_file_path = os.path.join(file_dir, filename)
 
             print("文件保存路径为：%s" % save_file_path)
-            # exit()
             df.set_index('open_time', drop=True, inplace=True)
             df.to_csv(save_file_path)
 
@@ -96,8 +87,6 @@ def sample_datas(exchange_name, symbol):
     path = os.path.join(os.getcwd(), exchange_name, symbol.replace('/', ''))
     print(path)
 
-    # exit()
-
     file_paths = []
     for root, dirs, files in os.walk(path):
         if files:
@@ -114,29 +103,11 @@ def sample_datas(exchange_name, symbol):
 
     all_df = all_df.sort_values(by='open_time', ascending=True)
 
-    print(all_df)
-
     return all_df
-
-    # for index, item in all_df.iterrows():
-    #     try:
-    #         dt = (pd.to_datetime(item['open_time'], unit='ms'))
-    #         print(dt)
-    #         dt = datetime.datetime.strptime(str(dt), '%Y-%m-%d %H:%M:%S')  # 2018-01-01 17:36:00:42
-    #         print(dt)
-    #     except:
-    #         dt = (pd.to_datetime(item['open_time'], unit='ms'))
-    #         print(dt)
 
 
 def clear_datas(exchange_name, symbol):
     df = sample_datas(exchange_name, symbol)
-    # print(df)
-    # exit()
-    # df['open_time'] = df['open_time'].apply(lambda x: time.mktime(x.timetuple()))
-    # # 日期.timetuple() 这个用法 通过它将日期转换成时间元组
-    # # print(df)
-    # df['open_time'] = df['open_time'].apply(lambda x: (x // 60) * 60 * 1000)
     df['open_time'] = df['open_time'].apply(lambda x: (x // 60) * 60)
     print(df)
     df['Datetime'] = pd.to_datetime(df['open_time'], unit='ms') + pd.Timedelta(hours=8)
@@ -149,11 +120,34 @@ def clear_datas(exchange_name, symbol):
 
 
 if __name__ == '__main__':
-    crawl_exchanges_datas('binance', 'BTC/USDT', '2018-1-1', '2018-2-1')
-    # crawl_exchanges_datas('bitmex', 'BTC/USD', '2018-1-1', '2018-3-1')
-    # crawl_exchanges_datas('bitfinex', 'BTC/USDT', '2018-1-1', '2019-7-22')
+    # crawl_exchanges_datas('coinbasepro', 'BTC/USD', '2018-1-1', '2019-7-22') #limit=300
+    # clear_datas('coinbase', 'BTC/USD')
 
-    # sample_datas('binance', 'BTC/USDT')
-    clear_datas('binance', 'BTC/USDT')
+    # crawl_exchanges_datas('binance', 'BTC/USDT', '2019-1-1', '2020-1-1') #yes
+    # clear_datas('binance', 'BTC/USDT')
 
+    # crawl_exchanges_datas('okex', 'BTC/USDT', '2019-1-1', '2019-3-1')  # limit=200
+    # clear_datas('okex', 'BTC/USDT')
+
+    crawl_exchanges_datas('bitfinex', 'BTC/USDT', '2020-4-1', '2020-4-17')  # limit=110000
+    clear_datas('bitfinex', 'BTC/USDT')
+
+    # crawl_exchanges_datas('bitstamp', 'BTC/USD', '2019-1-1', '2020-1-1')  # limit=57
+    # clear_datas('bitstamp', 'BTC/USD')
+
+    # crawl_exchanges_datas('bittrex', 'BTC/USDT', '2019-1-1', '2020-1-1')  #limit=5000
+    # clear_datas('bittrex', 'BTC/USDT')
+
+    # crawl_exchanges_datas('bitflyer', 'BTC/USD', '2019-1-1', '2020-1-1')  #limit=40
+    # clear_datas('bitflyer', 'BTC/USD')
+
+    # crawl_exchanges_datas('poloniex', 'BTC/USDT', '2019-1-1', '2020-1-1')  # limit=110000,timeframe=5m
+    # clear_datas('poloniex', 'BTC/USDT')
+
+
+    # crawl_exchanges_datas('gateio', 'BTC/USDT', '2019-1-1', '2020-1-1')  # limit=1000
+    # clear_datas('gateio', 'BTC/USDT')
+
+    # crawl_exchanges_datas('huobipro', 'BTC/USDT', '2019-1-1', '2020-1-1')  # limit=1000
+    # clear_datas('huobipro', 'BTC/USDT')
     pass
--- src/test_ccxt.py
+++ src/test_ccxt.py
@@ -15,38 +15,55 @@ poloniex = ccxt.poloniex()
 coincheck = ccxt.coincheck()
 gateio = ccxt.gateio()
 bitstamp = ccxt.bitstamp()
-# huobi.load_markets()  # BTC/USDT
-# print(huobi.symbols)
-
-# coincheck.load_markets()  # BTC/USDT
-# print(coincheck.symbols)
-
-
+# market = huobi.load_markets()  # BTC/USDT
+# print(huobi.market)
 symbol = 'BTC/USDT'
-# huobi_btc_ticker = huobi.fetch_ticker('BTC/USDT')
-# binance_btc_ticker = binance.fetch_ticker('BTC/USDT')
+
+# 实时行情：获取聚合行情（ticker）数据同时提供最近24小时的交易聚合信息
+huobi_btc_ticker = huobi.fetch_ticker('BTC/USDT')
+binance_btc_ticker = binance.fetch_ticker('BTC/USDT')
 # print(huobi_btc_ticker)
 # print(binance_btc_ticker)
 
 
+# ohlcv数据
 ohlcv_huobi = huobi.fetch_ohlcv(symbol, timeframe='15m')
 ohlcv_binance = binance.fetch_ohlcv(symbol, timeframe='15m')
-ohlcv_okex = okex.fetch_ohlcv(symbol, timeframe='15m')
-ohlcv_coinbase = coinbase.fetch_ohlcv('BTC/USD', timeframe='15m')
+ohlcv_okex = okex.fetch_ohlcv(symbol, timeframe='1m')
+ohlcv_coinbase = coinbase.fetch_ohlcv('BTC/USD', timeframe='1m')
 ohlcv_bitfinex = bitfinex.fetch_ohlcv(symbol, timeframe='15m')
-ohlcv_kraken = kraken.fetch_ohlcv(symbol, timeframe='15m')
+# ohlcv_kraken = kraken.fetch_ohlcv(symbol, timeframe='15m')
 ohlcv_bittrex = bittrex.fetch_ohlcv(symbol, timeframe='30m')
 ohlcv_bitflyer = bitflyer.fetch_ohlcv('BTC/USD', timeframe='15m')
 ohlcv_poloniex = poloniex.fetch_ohlcv(symbol, timeframe='15m')
 ohlcv_coincheck = coincheck.fetch_ohlcv('BTC/JPY', timeframe='15m')
 ohlcv_gateio = gateio.fetch_ohlcv(symbol, timeframe='15m')
-ohlcv_bitstamp = bitstamp.fetch_ohlcv(symbol, timeframe='15m')
+
+# 深度信息
+limit = 10
+depth_huobi = huobi.fetch_order_book(symbol, limit)
+# print(depth_huobi)
+
+# 获取当前的最好价格（查询市场价格）并且计算买入卖出的价差
+orderbook = huobi.fetch_order_book(symbol)
+bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
+ask = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
+spread = (ask - bid) if (bid and ask) else None
+# print(huobi.id, 'market price', {'bid': bid, 'ask': ask, 'spread': spread})
+
+
+# 获取指定交易对的最近交易记录
+huobi_trade = huobi.fetch_trades(symbol=symbol)
+# print(huobi_trade)
+
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 5000)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
-
-# df_huobi = pd.DataFrame(ohlcv_huobi, columns=["timestamp", "open_price", "high_price", "low_price", "closing_price", "amount", "vol"])
+#
+# df_huobi = pd.DataFrame(ohlcv_huobi,
+#                         columns=["timestamp", "open_price", "high_price", "low_price", "closing_price", "amount",
+#                                  "vol"])
 # df_huobi['timestamp'] = pd.to_datetime(df_huobi['timestamp'], unit='ms') + pd.Timedelta(hours=8)
 # print(df_huobi)
 
@@ -54,7 +71,8 @@ pd.set_option('display.float_format', lambda x: '%.3f' % x)
 # df_binance['timestamp'] = pd.to_datetime(df_binance['timestamp'], unit='ms') + pd.Timedelta(hours=8)
 # print(df_binance)
 
-# df_okex = pd.DataFrame(ohlcv_okex, columns=["timestamp", "open_price", "high_price", "low_price", "closing_price", "amount"])
+# df_okex = pd.DataFrame(ohlcv_okex,
+#                        columns=["timestamp", "open_price", "high_price", "low_price", "closing_price", "amount"])
 # df_okex['timestamp'] = pd.to_datetime(df_okex['timestamp'], unit='ms') + pd.Timedelta(hours=8)
 # print(df_okex)
 
@@ -62,10 +80,10 @@ pd.set_option('display.float_format', lambda x: '%.3f' % x)
 # df_coinbase['timestamp'] = pd.to_datetime(df_coinbase['timestamp'], unit='ms') + pd.Timedelta(hours=8)
 # print(df_coinbase)
 
-# df_bitfinex = pd.DataFrame(ohlcv_bitfinex,
-#                            columns=["timestamp", "open_price", "high_price", "low_price", "closing_price", "amount"])
-# df_bitfinex['timestamp'] = pd.to_datetime(df_bitfinex['timestamp'], unit='ms') + pd.Timedelta(hours=8)
-# print(df_bitfinex)
+df_bitfinex = pd.DataFrame(ohlcv_bitfinex,
+                           columns=["timestamp", "open_price", "high_price", "low_price", "closing_price", "amount"])
+df_bitfinex['timestamp'] = pd.to_datetime(df_bitfinex['timestamp'], unit='ms') + pd.Timedelta(hours=8)
+print(df_bitfinex)
 
 # df_kraken = pd.DataFrame(ohlcv_kraken,
 #                          columns=["timestamp", "open_price", "high_price", "low_price", "closing_price", "vol"])
@@ -95,13 +113,13 @@ pd.set_option('display.float_format', lambda x: '%.3f' % x)
 # print(df_coincheck)
 
 # 期货
-# exchange = ccxt.okex()
-# exchange.load_markets()
-# for symbol in exchange.markets:
-#     market = exchange.markets[symbol]
-#     # print(market.keys())
-#     if market['futures']:
-#         print('----------------------------------------------------')
-#         print(symbol, exchange.fetch_ticker(symbol))
-#         time.sleep(exchange.rateLimit / 1000)
+exchange = ccxt.okex()
+exchange.load_markets()
+for symbol in exchange.markets:
+    market = exchange.markets[symbol]
+    # print(market.keys())
+    if market['futures']:
+        print('----------------------------------------------------')
+        print(symbol, exchange.fetch_ticker(symbol))
+        time.sleep(exchange.rateLimit / 1000)
 #
